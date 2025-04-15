7b4227a7058b54b1e9e74b43da07ff22d5fbce54
Nafiz al din <siriusal121211@gmail.com>
2025-04-15 06:18:18 +0600

    library

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Library mode configuration
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/TiptapEditor.jsx'),
+      name: 'TiptapEditor',
+      // Provide file names for different formats
+      fileName: (format) => `tiptap-react-editor.${format}.js`,
+    },
+    rollupOptions: {
+      // Make sure to externalize peer dependencies:
+      external: ['react', 'react-dom', '@tiptap/react'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@tiptap/react': 'TiptapReact',
+        },
+      },
+    },
+  },
 })
